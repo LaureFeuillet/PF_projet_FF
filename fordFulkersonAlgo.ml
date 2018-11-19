7af7adb36b89e19c38adbd5ac9bc7e60f864c6ef
MaelPlantec <mael.plantec@gmail.com>
2018-11-19 13:27:49 +0100

    Fichiers fordfulkerson

--- fordFulkersonAlgo.ml
+++ fordFulkersonAlgo.ml
@@ -7,4 +7,19 @@ type 'a graph = (id * 'a out_arcs) list
 
 let empty_graph = []
 
+(**************  INITIALISATION  **************)
+
+(* Fonction permettant le passage d'un "capacité graph" à un "(flot * capacité) graph". *)
 let init_graph gr = Graph.map gr (label -> (0, label))
+
+(**************  PARCOURS  **************)
+
+
+
+(**************  MISE A JOUR  **************)
+
+
+
+(**************  FORD-FULKERSON  **************)
+
+

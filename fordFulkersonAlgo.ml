36099afd40448c48c09cdb9f3f0974271910aa1d
MaelPlantec <mael.plantec@gmail.com>
2018-11-19 13:24:09 +0100

    Fichiers fordfulkerson

--- fordFulkersonAlgo.ml
+++ fordFulkersonAlgo.ml
@@ -5,3 +5,6 @@ type 'a out_arcs = (id * 'a) list
 (* A graph is just a list of pairs: a node & its outgoing arcs. *)
 type 'a graph = (id * 'a out_arcs) list
 
+let empty_graph = []
+
+let init_graph gr = Graph.map gr (label -> (0, label))
--- fordFulkersonAlgo.mli
+++ fordFulkersonAlgo.mli
@@ -4,4 +4,17 @@ type 'a graph
 (* Each node has a unique identifier (a name). *)
 type id = string
 
+(* The empty graph. *)
+val empty_graph: 'a graph
+
+(**************  INITIALISATION  **************)
+val init_graph: 'a graph -> 'b graph
+
+(**************  PARCOURS  **************)
+
+
+(**************  MISE A JOUR  **************)
+
+
+(**************  FORD-FULKERSON  **************)
 

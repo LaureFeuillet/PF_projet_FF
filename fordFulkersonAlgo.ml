6faa9274a8610cb6c886651884cc34e5a7978e69
MaelPlantec <mael.plantec@gmail.com>
2018-11-19 13:16:03 +0100

    Fichiers fordfulkerson

--- fordFulkersonAlgo.ml
+++ fordFulkersonAlgo.ml
@@ -4,3 +4,4 @@ type 'a out_arcs = (id * 'a) list
 
 (* A graph is just a list of pairs: a node & its outgoing arcs. *)
 type 'a graph = (id * 'a out_arcs) list
+
--- fordFulkersonAlgo.mli
+++ fordFulkersonAlgo.mli
@@ -1,3 +1,7 @@
-open Graph
+(* Type of a directed graph in which arcs have labels of type 'a. *)
+type 'a graph
 
-type path = string
+(* Each node has a unique identifier (a name). *)
+type id = string
+
+

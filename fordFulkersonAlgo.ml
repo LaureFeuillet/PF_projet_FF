19d8f4d16607598a5851c72eb75ff6f5bcb05c48
Mme Feuillet Laure <feuillet@insa-08129>
2018-11-22 10:53:48 +0100

    Initialisation et début du graphe d'écart

--- fordFulkersonAlgo.ml
+++ fordFulkersonAlgo.ml
@@ -1,19 +1,20 @@
-type id = string
+open Graph
 
-type 'a out_arcs = (id * 'a) list
-
-(* A graph is just a list of pairs: a node & its outgoing arcs. *)
-type 'a graph = (id * 'a out_arcs) list
-
-let empty_graph = []
+type fc = {
+	flow : int ; 
+	capacity : int } 
 
 (**************  INITIALISATION  **************)
 
 (* Fonction permettant le passage d'un "capacité graph" à un "(flot * capacité) graph". *)
-let init_graph gr = Graph.map gr (label -> (0, label))
+let init_graph gr = Graph.map gr (capacity -> (0, capacity))
 
 (**************  PARCOURS  **************)
-
+(* Construct a residual graph from a capacity graph *)
+let residual_graph gr = 
+	let result = empty_graph in 
+	Graph.map gr 
+	
 
 
 (**************  MISE A JOUR  **************)
--- fordFulkersonAlgo.mli
+++ fordFulkersonAlgo.mli
@@ -1,17 +1,18 @@
-(* Type of a directed graph in which arcs have labels of type 'a. *)
-type 'a graph
+open Graph
 
-(* Each node has a unique identifier (a name). *)
-type id = string
-
-(* The empty graph. *)
-val empty_graph: 'a graph
+type fc = {
+	flow : int ; 
+	capacity : int } 
 
 (**************  INITIALISATION  **************)
-val init_graph: 'a graph -> 'b graph
+(* Construct a flow graph from a given capacity graph *)
+val init_graph: int graph -> fc graph
 
 (**************  PARCOURS  **************)
-val residual_graph: 'a graph -> 'b graph
+(* Construct a residual graph from a capacity graph *)
+val residual_graph: fc graph -> int graph
+
+(* Tour a residual graph to find a path from source to sink, and its minimal cost *)
 val tour_graph: 'a graph -> 'a graph * int
 
 (**************  MISE A JOUR  **************)
@@ -20,6 +21,7 @@ val update_path:
 
 val add_path_to_graph:
 
+(*  *)
 val update_graph: 'a graph -> 'b graph -> int -> 'b graph
 
 (**************  FORD-FULKERSON  **************)

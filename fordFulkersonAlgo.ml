f9f36553c107d480d99cb3a5edc13b80ab25a670
unknown <mael.plantec@gmail.com>
2018-11-22 21:32:05 +0100

    residual worjking ?

--- fordFulkersonAlgo.ml
+++ fordFulkersonAlgo.ml
@@ -17,14 +17,24 @@ let graph_without_arcs gr =
 	let f acu id out_arcs = add_node acu id in
 	v_fold gr f result
 
+
+let rec f g sommet = 
+  match g with
+  [] -> failwith "Accès à un sommet non existant"
+ |(s, voisins)::reste -> if s = sommet then 
+                               voisins
+                        else  
+                               getVoisins reste sommet;;
+
+
 (* Construct a residual graph from a capacity graph *)
 let residual_graph gr = 
 	let result = graph_without_arcs gr in
 	(* Je veux que cette fonction regarde chaque arc et crée le ou les arcs qui correspondent dans le graphe d'écart. *)
 	let rec f acu id out_arcs = match out_arcs with
-		| (idD,{flow = 0; capacity = capa})::tail -> f (add_arc acu id idD capa) id tail
-		| (idD,{flow = capa; capacity = capa})::tail -> f (add_arc acu idD id capa) id tail
-		| (idD,{flow = flot; capacity = capa})::tail -> f (add_arc (add_arc acu idD id flot) id idD (capa-flot)) id tail
+		| (idD,{flow = 0; capacity})::tail -> f (add_arc acu id idD capacity) id tail
+		| (idD,{flow = flot; capacity = capa})::tail -> if flot == capa then f (add_arc acu idD id flot) id tail 
+	else f (add_arc (add_arc acu idD id flot) id idD (capa-flot)) id tail  	
 	in
 	v_fold gr f result
 	
@@ -37,3 +47,4 @@ let update_graph gr path cost = assert false
 
 (**************  FORD-FULKERSON  **************)
 
+ 
